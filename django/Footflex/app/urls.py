790421164a57a36a3694858ddb0f561eb8caf188
sreerajpr33 <prsreeraj33@gmail.com>
2024-11-29 16:45:48 +0530

    main

--- django/Footflex/app/urls.py
+++ django/Footflex/app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns=[
-    path('',views.login)
+    path('',views.login),
+    path('shophome',views.shop_home),
+    path('register',views.register),
 ]
--- django/Footflex/app/views.py
+++ django/Footflex/app/views.py
@@ -1,6 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login,logout
+from . models import*
+from django.contrib import messages
 
 # Create your views here.
+def shop_home(req):
+    if 'shop' in req.session:
+        data=Product.objects.all()
+        return render(req,'shop/shophome.html',{'products':data})
+    else:
+        return redirect(login)
 
 def login(req):
-    return render(req,'Login.html')
+    if 'shop' in req.session:
+        return redirect(shop_home)
+    if 'user' in req.session:
+        return redirect(user_home)
+    if req.method=='POST':
+        uname=req.POST['uname']
+        password=req.POST['password']
+        data=authenticate(username=uname,password=password)
+        if data:
+            login(req,data)
+            if data.is_superuser:
+                req.session['shop']=uname
+                return redirect(shop_home)
+            else:
+                req.session['user']=uname
+                return redirect(user_home)
+        else:
+            messages.info(req, " ")
+            return redirect(login)
+    else:
+        return render(req,'Login.html')
+
+
+# --------------------user-------------------
+
+def user_home(req):
+    return render(req,'user/user_home.html')
+
+def register(req):
+    return render(req,'user/register.html')
